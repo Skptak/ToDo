b4e7c821c1ddd6ba94ce711ed45f9981f0a2d2e6
Skptak <Skptak@outlook.com>
2017-06-02 15:49:40 -0400

    Commit

--- src/main/groovy/com/example/ToDo/UserService.groovy
+++ src/main/groovy/com/example/ToDo/UserService.groovy
@@ -1,20 +1,11 @@
 package com.example.ToDo
 
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.dao.DataIntegrityViolationException
-import org.springframework.data.domain.Page
-import org.springframework.data.domain.Pageable
-import org.springframework.data.mongodb.core.MongoTemplate
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories
-import org.springframework.stereotype.Repository
 import org.springframework.stereotype.Service
 
 /**
  * Created by Soren Ptak on 6/1/2017.
  */
-//@EnableMongoRepositories(basePackages = "com.example.ToDo")
-//@Service
-
 @Service
 class UserService{
     @Autowired
